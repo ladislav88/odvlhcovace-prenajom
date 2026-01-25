6db59c42ab14222eb76462d499b990b141838284
Ladislav Husár <ladislav.husar@icloud.com>
2026-01-25 15:48:18 +0100

    auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export const metadata: Metadata = {
   title: "Prenájom odvlhčovačov Trenčín | Osobný odber",
   description: "Prenájom výkonných odvlhčovačov v Trenčíne. Ideálne po zatopení, do pivníc, bytov a novostavieb. Osobný odber po dohode.",
   icons: {
-    icon: '/images/odvlhcovac1-cfcd4dcc-1c2c-4747-a1ef-865033e6baa4.png',
+    icon: [
+      { url: '/images/odvlhcovac1-cfcd4dcc-1c2c-4747-a1ef-865033e6baa4.png', sizes: 'any' },
+    ],
     shortcut: '/images/odvlhcovac1-cfcd4dcc-1c2c-4747-a1ef-865033e6baa4.png',
     apple: '/images/odvlhcovac1-cfcd4dcc-1c2c-4747-a1ef-865033e6baa4.png',
   },
